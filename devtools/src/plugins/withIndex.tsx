2083c24ecb2e71617658f2534a212e4e7cc94a14
Nivekithan S <nivekithan363@gmail.com>
2021-02-25 11:31:10 +0530

    WithIndex plugin has been created

---
+++ devtools/src/plugins/withIndex.tsx
@@ -0,0 +1,33 @@
+import { NodeEntry, Element, Editor, Transforms } from "slate";
+import { ReactEditor } from "slate-react";
+
+export const withIndex = (editor: ReactEditor): ReactEditor => {
+  const { normalizeNode } = editor;
+
+  editor.normalizeNode = (entry) => {
+    const [node, path] = entry;
+
+    if (!Editor.isEditor(node) && !hasValidIndex(entry)) {
+      Transforms.setNodes(
+        editor,
+        { devtools_index: path[path.length - 1] },
+        { at: path }
+      );
+    }
+    normalizeNode(entry);
+  };
+
+  return editor;
+};
+
+const hasValidIndex = ([node, path]: NodeEntry): boolean => {
+  const { devtools_index } = node;
+
+  if (!devtools_index) return false;
+
+  if (typeof devtools_index !== "number") return false;
+
+  if (devtools_index !== path[path.length - 1]) return false;
+
+  return true;
+};
