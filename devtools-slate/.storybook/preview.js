ea865410a3aca5944a757aa30f4dbc4e72813008
Nivekithan S <nivekithan363@gmail.com>
2021-03-08 18:18:38 +0530

    (css) : installed storybook

---
+++ devtools-slate/.storybook/preview.js
@@ -0,0 +1,16 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    default: "dark",
+    values: [
+      {
+        name: "dark",
+        value: "#282a36",
+      },
+      {
+        name: "white",
+        value: "#fff",
+      },
+    ],
+  },
+};
