047d6e26ac7d8ca7e0203d3b8142da1d6be56334
Nivekithan S <nivekithan363@gmail.com>
2021-03-15 10:08:59 +0530

    (master) : fixed bug which causing infinite rerenders

--- slate-devtools/src/devtools/updateButtons.tsx
+++ slate-devtools/src/devtools/updateButtons.tsx
@@ -98,9 +98,9 @@ export const UpdateButtons = ({ editor, value, devValue }: Props) => {
   useEffect(() => {
     const { current } = appOperations;
 
-    if (current.length !== 0) {
+    if (current.length !== 0 && updateDevtools !== "on") {
       setUpdateDevtools("on");
-    } else {
+    } else if (current.length === 0 && updateDevtools !== "off") {
       setUpdateDevtools("off");
     }
   });
@@ -112,10 +112,10 @@ export const UpdateButtons = ({ editor, value, devValue }: Props) => {
   useEffect(() => {
     const { current } = devtoolsOperations;
 
-    if (current.length !== 0) {
+    if (current.length !== 0 && updateApp !== "on") {
       setUpdateApp("on");
       clickUpdateAppButtonOnce();
-    } else {
+    } else if (current.length === 0 && updateApp !== "off") {
       setUpdateApp("off");
     }
   });
